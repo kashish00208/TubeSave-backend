a7b0412530484798246493c09b5ee45570adfdb8
kashish00208 <kgupta7827@gmail.com>
2025-07-30 15:05:51 +0530

    deployment fix

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ print("started working now")
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
